0b9561bd411a6c064777acbecd8900aabd7510cd
HASSAN PROGRAMMER 256 <hassanprogrammer256@gmail.com>
2025-03-02 14:38:53 +0300

    correct

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173' // Adjust this according to your frontend origin
+  origin: 'https://minifygadgets.netlify.app' // Adjust this according to your frontend origin
 }));
 // app.use(cors({origin:'https://localhost:5173',methods:['POST','GET'],credentials:true}))
 let emailVerifications = [];
