a744209efaa5795d457101c996977bebcb95c812
HASSAN PROGRAMMER 256 <hassanprogrammer256@gmail.com>
2025-03-05 14:36:11 +0300

    hgjt

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -3,9 +3,10 @@ import React, { useContext } from 'react';
 import { AppContext } from './AppContext';
 
 const Loading = () => {
+  const {fullpageloading,isLoading} = useContext(AppContext)
   return (
 
-      <div className="fixed top-0 left-0 w-full h-full flex bg-modal justify-center items-center z-30">
+      fullpageloading ? <div className="fixed top-0 left-0 w-full h-full flex bg-modal justify-center items-center z-30">
         <div className="flex justify-center flex-col text-white font-black gap-6">
           <h1 className="text-white text-7xl font-black text-center">PLEASE WAIT......</h1>
           <div className="d-flex justify-content-center align-items-center">
@@ -14,10 +15,12 @@ const Loading = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> : isLoading ? <div className="flex justify-center">
+        <div className="spinner-border-lg"></div>
+      </div> : null
 
     
   );
-}
+} 
 
 export default Loading;
